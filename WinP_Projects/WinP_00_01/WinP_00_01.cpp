902af447bbb0ea121c309e2a2d0091e14b4600a5
Seojin.K <7701seojin@gmail.com>
2026-03-06 02:24:50 +0900

    윈플 1 for문으로 변경
    필요없는 while문 for문으로 변경

--- WinP_Projects/WinP_00_01/WinP_00_01.cpp
+++ WinP_Projects/WinP_00_01/WinP_00_01.cpp
@@ -12,14 +12,14 @@ string change_star(string, char, string);
 string space_random(string);
 void word(string);
 string change_char(string);
-string left(string);
-string right(string);
+string move_left(string);
+string move_right(string);
 
 int main() {
 	string sentence, result_sentence;
 
 	cout << "input sentence: ";
-	while (1) {
+	while (true) {
 		string input_sentence{};
 		getline(cin, input_sentence, '.');
 
@@ -37,103 +37,93 @@ int main() {
 
 	result_sentence = sentence;
 
-	while (1) {
+	bool out = true;
+	while (out) {
 		cout << "명령어: ";
-		char n;
-		cin >> n;
+		char cmd;
+		cin >> cmd;
 
-		if ('a' <= n && n <= 'z') {
-			sentence = change_star(sentence, n, result_sentence);
-			cout << sentence << '\n';
-		}
-		else if (n == '1') {
+		switch (cmd) {
+		case '1':
 			sentence = space_random(sentence);
 			cout << sentence << '\n';
-		}
-		else if (n == '2')
+			break;
+		case '2':
 			word(result_sentence);
-		else if (n == '3') {
+			break;
+		case '3':
 			sentence = change_char(sentence);
 			cout << sentence << '\n';
-		}
-		else if (n == '4') {
-			sentence = left(sentence);
-			cout << sentence << '\n';
-		}
-		else if (n == '5') {
-			sentence = right(sentence);
-			cout << sentence << '\n';
-		}
-		else if (n == '0')
 			break;
-		else {
-			cout << "command error" << '\n';
-			continue;
+		case '4':
+			sentence = move_left(sentence);
+			cout << sentence << '\n';
+			break;
+		case '5':
+			sentence = move_right(sentence);
+			cout << sentence << '\n';
+			break;
+		case '0':
+			out = false;
+			break;
+		default:
+			if (('a' <= cmd && cmd <= 'z') || ('A' <= cmd && cmd <= 'Z')) {
+				sentence = change_star(sentence, cmd, result_sentence);
+				cout << sentence << '\n';
+			}
+			else
+				cout << "command error" << '\n';
+			break;
 		}
 	}
 }
 
-string change_star(string str, char n, string def_str)	// 입력 문자(a~z)를 *로 바꾸고 출력
+string change_star(string str, char c, string def_str)	// 입력 문자(a~z)를 *로 바꾸고 출력
 {
-	string star_str;
-	int i = 0;
-
-	while (i < str.size()) {
-		if (str[i] == n || str[i] == n - 32)
-			star_str.push_back('*');
+	for (int i = 0; i < str.length(); ++i) {
+		if ((toupper(c) == toupper(str[i])) || (tolower(c) == tolower(str[i])))
+			str[i] = '*';
 		else if (str[i] == '*')
 			return def_str;
-		else
-			star_str.push_back(str[i]);
-		++i;
 	}
 
-	return star_str;
+	return str;
 }
 
 string space_random(string str)	// 공백에 랜덤 문자 삽입
 {
-	string random;
-	int i = 0;
-
-	while (i < str.size()) {
+	for (int i = 0; i < str.length(); ++i) {
 		if (str[i] == ' ') {
-			if (uid(dre) == 1)
-				random.push_back('@');
-			else if (uid(dre) == 2)
-				random.push_back('#');
-			else if (uid(dre) == 3)
-				random.push_back('%');
-			else if (uid(dre) == 4)
-				random.push_back('.');
-			else {
-				random.push_back('.');
+			switch (uid(dre)) {
+			case 1: 
+				str[i] = '@';
+				break;
+			case 2:
+				str[i] = '#';
+				break;
+			case 3:
+				str[i] = '%';
+				break;
+			case 4:
+				str[i] = '.';
+				break;
 			}
 		}
-		else if (str[i] != ' ') {
-			if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
-				random.push_back(str[i]);
-			else if (str[i] == '@' || str[i] == '#' || str[i] == '%' || str[i] == '.') {
-				if (i == str.size() - 1)
-					random.push_back(str[i]);
-				else
-					random.push_back(' ');
-			}
+		else if (str[i] == '@' || str[i] == '#' || str[i] == '%' || str[i] == '.') {
+			if (i != str.length() - 1)
+				str[i] = ' ';
 		}
-		++i;
 	}
 
-	return random;
+	return str;
 }
 
 void word(string str)	// 단어 개수
 {
-	int cnt{ 0 }, i{ 0 };
-
-	while (i < str.size()) {
-		if (str[i] == ' ')
+	int cnt{};
+	for (int i = 1; i < str.length(); ++i) {
+		if (str[i] == ' ' && str[i - 1] != ' ')
 			++cnt;
-		++i;
 	}
 
 	cout << cnt + 1 << "words" << '\n';
@@ -141,87 +131,40 @@ void word(string str)	// 단어 개수
 
 string change_char(string str)	// 소문자->대문자&대문자->소문자
 {
-	string C;
-	int i{ 0 };
-
-	while (i < str.size()) {
-		if (str[i] >= 'a' && str[i] <= 'z') {
-			C.push_back(toupper(str[i]));
-		}
-		else if (str[i] >= 'A' && str[i] <= 'Z') {
-			C.push_back(tolower(str[i]));
-		}
-		else C.push_back(str[i]);
-
-		++i;
+	for (int i = 0; i < str.length(); ++i){
+		if (isupper(str[i]))
+			str[i] = tolower(str[i]);
+		else
+			str[i] = toupper(str[i]);
 	}
 
-	return C;
+	return str;
 }
 
-string left(string str)	// 좌측으로 한 문자씩 이동
+string move_left(string str)	// 좌측으로 한 문자씩 이동
 {
-	string lstr;
-	int i{ 0 };
+	str.push_back(str.front());
+	str.erase(str.begin());
 
-	while (i < str.size()) {
-		if (i == str.size() - 1) {
-			lstr.push_back(str[i]);
-		}
-		else if (i == str.size() - 2) {
-			lstr.push_back(str[0]);
-		}
-		else {
-			lstr.push_back(str[i + 1]);
-		}
-		++i;
-	}
-
-	return lstr;
+	return str;
 }
 
-string right(string str)	// 우측으로 한 문자씩 이동
+string move_right(string str)	// 우측으로 한 문자씩 이동
 {
-	string rstr;
-	int i{ 0 };
+	str.insert(str.begin(), str.back());
+	str.pop_back();
 
-	while (i < str.size()) {
-		if (i == str.size() - 1) {
-			rstr.push_back(str[i]);
-		}
-		else if (i == 0) {
-			rstr.push_back(str[str.size() - 2]);
-		}
-		else {
-			rstr.push_back(str[i - 1]);
-		}
-
-		++i;
-	}
-
-	return rstr;
+	return str;
 }
 
 string space(string str)
 {
-	int i{ 0 };
-	string result;
-
-	while (i < str.size()) {
-		if (str[i] == ' ') {
-			if (str[i - 1] != ' ' && i != 0) {
-				result.push_back(str[i]);
-			}
-			else {
-				++i;
-				continue;
-			}
-		}
-		else result.push_back(str[i]);
-		++i;
+	for (int i = 1; i < str.length(); ++i) {
+		if (str[i] == ' ' && str[i - 1] == ' ')
+			str.erase(i--, 1);
 	}
 
-	return result;
+	return str;
 }
 
 // [1차원 배열 사용] 문자열 다루기
